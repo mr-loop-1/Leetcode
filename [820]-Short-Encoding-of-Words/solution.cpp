e88479bbe26e3dc9fbf8d084e6fc7a1f5a230a3b
Abdul Samad <62374784+mr-loop-1@users.noreply.github.com>
2022-06-20 11:38:38 +0530

    Update solution.cpp

--- [820]-Short-Encoding-of-Words/solution.cpp
+++ [820]-Short-Encoding-of-Words/solution.cpp
@@ -14,19 +14,9 @@ int minimumLengthEncoding(vector<string>& words) {
         
         for(auto word: words) {
             int size = word.size();
-            // cout << word << endl << endl;
-            
-            // bool flag = 1;
-            // for(auto s: suff) {
-            //     // cout << s << endl;
-            //     if(s.substr(s.size()-size, size) == word) {
-            //         flag = 0;
-            //     }
-            // }
+
             if(!(has.count(word))) {
-            // if(flag) {
-                // suff.push_back(word);
-                
+
                 ans += size;
                 while(size--) {
                     has[word] = true;
