1159294302bc634b0e171ea9ab008f4433df1ac9
Abdul Samad <62374784+mr-loop-1@users.noreply.github.com>
2022-06-20 11:39:29 +0530

    Update solution.cpp

--- [2311]-Longest-Binary-Subsequence-Less-Than-or-Equal-to-K/solution.cpp
+++ [2311]-Longest-Binary-Subsequence-Less-Than-or-Equal-to-K/solution.cpp
@@ -17,51 +17,17 @@ int longestSubsequence(string s, int k) {
         for(int i=size-1, j=size-1; i!=-1; i--) {
             
             bitset<32> bt(s[i]+ss);
-            
-            // cout << "i " << i << " j " <<  j <<  " Bt " << bt << endl;
-            
+
             if(bt.to_ulong() > k) {
                 bt = bitset<32>(ss);
             }
             else {
                 ss = s[i]+ss;
             }
-
-            
-            // cout << "Tt " << bt << endl;
-            
-            // if(bt.to_ulong() <= k) {
             
             ans = max(ans, zeroLeft[i]+(int)ss.size());
             
-            // }
-            
-            // cout << "ans " << ans << endl;
-            
         }
-        
-//         for(int i=size-1, j=size-1; i!=-1; i--) {
-//             bitset<32> bt(s.substr(i, j-i+1));
-            
-//             cout << "i " << i << " j " <<  j <<  " Bt " << bt << endl;
-            
-//             while(bt.to_ulong() > k) {
-//                 j--;
-//                 bitset<32> tt(s.substr(i, j-i+1));
-//                 bt = tt;
-//             }
-            
-//             cout << "Tt " << bt << endl;
-            
-//             if(bt.to_ulong() <= k) {
-//                 ans = max(ans, zeroLeft[i]+j-i+1);
-//             }
-            
-//             cout << "ans " << ans << endl;
-            
-//         }
-        
-        
         
         return ans;
         
